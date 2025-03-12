e8cfe157ec80dc3b53ea8d8232843ae59489de33
mashkinsa <mariasmolovik1@gmail.com>
2025-03-12 17:30:47 +0700

    second commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+    }
+ })
+ 
