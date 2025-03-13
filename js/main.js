1966ecf760e1f8a36787e602f30e85f936d03fcb
mashkinsa <mariasmolovik1@gmail.com>
2025-03-13 16:34:03 +0700

    eighth commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,57 @@
-let app = new Vue({
-    el: '#app',
-    data: {
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    }, 
+    template: `
+    <div class="product">
+        <div class="product-image">
+           <img :src="image" :alt="altText" />
+        </div>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+           <p>{{ sale }}</p>
+            <a :href="link">More products like this</a>
+            <p v-if="inStock">In Stock</p>
+            <p v-if="!inStock" :class="{ 'out-of-stock': !inStock }">Out of Stock</p>
+            <span v-if="onSale">On Sale</span>
+
+            <product-details :details="details"></product-details>
+
+            <p>User is premium: {{ premium }}</p>
+            <p>Shipping: {{ shipping }}</p>
+            <div
+                class="color-box"
+                v-for="(variant, index) in variants"
+                :key="variant.variantId"
+                :style="{ backgroundColor:variant.variantColor }"
+                @mouseover="updateProduct(index)"
+            ></div>
+             
+
+            <ul>
+                <li v-for="size in sizes" :key="size">{{ size }}</li>
+            </ul>
+
+            <div class="cart">
+                <p>Cart({{ cart }})</p>
+            </div>
+            <button
+                v-on:click="addToCart"
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock }"
+            >
+                Add to cart
+            </button>
+            <button v-on:click="removeFromCart">Remove from cart</button>
+        
+        </div>
+    </div>
+  `,
+    data() {
+        return { 
         product: "Socks",
         brand: 'Vue Mastery',
         selectedVariant: 0,
@@ -21,12 +72,12 @@ let app = new Vue({
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
                 variantQuantity: 0
             }
-         ],
-                 
+        ],
+                
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
         
-    },
+    }},
     methods: {
         addToCart() {
             this.cart += 1
@@ -39,7 +90,7 @@ let app = new Vue({
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
-         }, 
+        }, 
     },
     computed: {
         title() {
@@ -58,7 +109,35 @@ let app = new Vue({
                 return `${this.brand} ${this.product} are not on sale.`;
             }
         },
-     },
-   
+        shipping() {
+            if (this.premium) {
+                return "Free";
+            } else {
+                return 2.99
+            }
+        }
+ 
+    },
+    })
+    
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    `
+    });
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
+    }
  })
  
