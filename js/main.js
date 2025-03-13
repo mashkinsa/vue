a7886bceb9591bdfb94a8ad0c6634d30b45e5d38
mashkinsa <mariasmolovik1@gmail.com>
2025-03-13 15:19:28 +0700

    seventh commit

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -13,13 +13,16 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
-         ],         
+         ],
+                 
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
         
@@ -33,10 +36,29 @@ let app = new Vue({
                 this.cart -= 1
             }
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
-         },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+         }, 
     },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return `${this.brand} ${this.product} are on sale!`;
+            } else {
+                return `${this.brand} ${this.product} are not on sale.`;
+            }
+        },
+     },
    
  })
  
